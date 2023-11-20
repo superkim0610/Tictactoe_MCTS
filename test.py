0f2b71899cb0b259353c1b6eb1e499f8fca9b023
superkim0610 <81168407+superkim0610@users.noreply.github.com>
2023-11-21 00:30:41 +0900

    Tictactoe.py editted, for get score

--- test.py
+++ test.py
@@ -1,15 +1,27 @@
 from tictactoe import Tictactoe
 
-game = Tictactoe()
 
-while True:
-    state = game.state
-    for l in state:
-        print(" ".join(map(str, l)))
-    print('turn :', game.turn)
-    game.play(tuple(map(int, input().split())))
-    print('\n\n')
+def play_game_manual():
+    game = Tictactoe()
 
-    if not game.result == 0:
-        print("winner :", game.result)
-        break
+    while True:
+        state = game.state
+        for l in state:
+            print(" ".join(map(str, l)))
+        print('turn :', game.turn)
+        game.play(tuple(map(int, input().split())))
+        print('\n\n')
+
+        if not game.result == 0:
+            print("winner :", game.result)
+            break
+
+    for _ in game.notation:
+        print(_)
+    print(game.get_score())
+
+class Tree:
+    def __init__(self):
+        pass
+
+# play_game_manual()
--- tictactoe.py
+++ tictactoe.py
@@ -8,7 +8,7 @@ class Tictactoe:
         self.result = 0 # winner
         self.auto_check = True # automatically check() after play()
 
-    def play(self, pos: tuple, player=None) -> None:
+    def play(self, pos: tuple, player=None, is_notation=False) -> None:
         # player setting
         if player == None:
             player = self.turn
@@ -21,18 +21,19 @@ class Tictactoe:
             raise Exception("Player must be 1 or 2")
         
         # state check
-        if not self.state[pos[0]][pos[1]] == 0:
-            raise Exception("That position is already occupied")
+        self.check_pos(pos)
         
         # change game data
         self.state[pos[0]][pos[1]] = player
+        if not is_notation:
+            self.notation.append({"player": self.turn, "pos": pos})
         self.turn = 1 if player == 2 else 2
 
         # check game result
         if self.auto_check:
-            self.check()
+            self.check_end()
 
-    def check(self):
+    def check_end(self):
         # check horizontal
         for i in range(3):
             if self.state[0][i] == 0:
@@ -62,6 +63,43 @@ class Tictactoe:
             if self.state[0][2] == self.state[1][1] and self.state[0][2] == self.state[2][0]:
                 self.result = self.state[1][1]
                 return self.result
+            
+        # check draw
+        if len(self.notation) == 9:
+            self.result = 3
         
         return self.result
-        
+
+    def check_pos(self, pos):
+        if not self.state[pos[0]][pos[1]] == 0:
+            raise Exception("That position is already occupied")
+
+    def set_notation(self, notation):
+        self.state = [[0, 0, 0],
+                      [0, 0, 0],
+                      [0, 0, 0]]
+        self.notation = notation
+        self.result = 0
+
+        print(len(notation))
+        for n_notation in notation:
+            self.play(n_notation["pos"], player=n_notation["player"], is_notation=True)
+
+    def get_score(self, player=1):
+        SCORE_WIN = 30
+        SCORE_LOSE = -30
+        SCORE_DRAW = -10
+        SCORE_EACH_TURN = -2
+
+        score = 0
+        self.check_end()
+        if self.result == 3:
+            score += SCORE_DRAW
+        elif self.result == player:
+            score += SCORE_WIN
+        else:
+            score += SCORE_LOSE
+
+        score += SCORE_EACH_TURN * len(self.notation)
+
+        return score
